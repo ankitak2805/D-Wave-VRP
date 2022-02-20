ba7dc903ddfea35c1ff15256ff7739e56afb4603
ankitak2805 <36038344+ankitak2805@users.noreply.github.com>
2022-02-20 22:27:36 +0530

    Update vrptw_solver.py

--- src/vrptw_solver.py
+++ src/vrptw_solver.py
@@ -1,1 +1,8 @@
 
+class CQMSolver(VRPSolver):
+    def solve(self, )
+        qubo = self.problem.get_full_qubo(only_one_const, order_const)
+        sample = DWaveSolvers.CQMSolver(cqm, label='cqm-vrptw')
+        solution = VRPSolution(self.problem, sample)
+
+        return solution
